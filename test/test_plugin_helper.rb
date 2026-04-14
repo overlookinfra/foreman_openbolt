57aa8a1aa0b7e75027d4d37fedca0427d15191aa
nmburgan <13688219+nmburgan@users.noreply.github.com>
2026-04-14 01:52:28 +0000

    Move unit tests to test/unit/ directory
    Reorganize unit test files under test/unit/ so acceptance tests
    introduced later can live alongside in test/acceptance/ without
    ambiguity.
    
    Signed-off-by: nmburgan <13688219+nmburgan@users.noreply.github.com>

--- test/test_plugin_helper.rb
+++ test/test_plugin_helper.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 require 'dynflow/testing'
 
 # Load plugin factories and foreman-tasks factories
-FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
+FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), 'unit', 'factories')
 FactoryBot.definition_file_paths << File.join(ForemanTasks::Engine.root, 'test', 'factories')
 FactoryBot.reload
 
